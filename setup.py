83947f5e93c1bf0fe20ba483c39af75fae029fb9
Rajavi Mishra <59727634+rajavi-mishra@users.noreply.github.com>
2021-05-29 04:32:34 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup, Extension
+import sysconfig
+
+def main():
+    CFLAGS = ['-g', '-Wall', '-std=c99', '-fopenmp', '-mavx', '-mfma', '-pthread', '-O3']
+    LDFLAGS = ['-fopenmp']
+    # Use the setup function we imported and set up the modules.
+    # You may find this reference helpful: https://docs.python.org/3.6/extending/building.html
+    # TODO: YOUR CODE HERE
+    module1 = Extension('numc',
+                    sources = ['numc.c', 'matrix.c'],
+                    extra_compile_args = CFLAGS
+                    )
+
+    setup (name = 'numc',
+       version = '1.0',
+       description = 'matrix operations',
+       ext_modules = [module1]
+       )
+
+if __name__ == "__main__":
+    main()
